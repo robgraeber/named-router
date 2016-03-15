399031a36ab76455ef07687506e42f9ee1340f22
Rob Graeber <bgraeber@gmail.com>
2016-03-15 02:39:39 -0700

    Update named-routes lib

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var NamedRoutes = require('named-routes');
 var express = require('express');
 
-var createRouter = function() {
+var NamedRouter = function() {
     var router = express.Router();
     var namedRoutes = new NamedRoutes();
-    namedRoutes.extendExpress4(router);
+    namedRoutes.extendExpress(router);
     router.build = namedRoutes.build.bind(namedRoutes);
 
     return router;
 };
 
-module.exports = createRouter;
+module.exports = NamedRouter;
